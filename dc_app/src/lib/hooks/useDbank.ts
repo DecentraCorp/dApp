d99695afda3eafc3d35ded8763bf139bbdc4b8ef
Joshua-Jack <Joshuajack256@gmail.com>
2021-08-25 20:32:09 +0200

    Dbank

---
+++ dc_app/src/lib/hooks/useDbank.ts
@@ -0,0 +1,30 @@
+export function UseDbank () {
+
+    const _setup = () => {
+
+    }
+
+    const _purchaseStock = () => {
+
+    }
+
+    const _sellStock = () => {
+
+    }
+    const _addNewCollateralType = () => {
+
+    }
+    const _fundWithdrawl = () => {
+
+    }
+    const _calculatePoolBa = () => {
+
+    }
+    const _calculatePurchase = () => {
+
+    }
+    const _calculateSale = () => {
+
+    }    
+    return {}
+}
