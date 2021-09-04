693a072fcd14da1f09c4a199e1f8a31c701d9577
Joshua-Jack <Joshuajack256@gmail.com>
2021-09-04 16:46:52 +0300

    Testing Simple Queries

--- dc_app/src/lib/queries/proposalQueries.ts
+++ dc_app/src/lib/queries/proposalQueries.ts
@@ -3,12 +3,35 @@ import { gql, useQuery } from '@apollo/client';
 export const PROPOSAL_DATA = gql`
 query Proposal($id: ID!){
     id
-    creator
+    creator {
+        id
+    }
     target
     timeCreated
     executed 
     proposalPassed
     proposalHash
-    callData
+    votes
 }
 `
+
+export const VOTE_DATA = gql`
+query Proposal($id: ID!){
+    id
+    account {
+        id
+    }
+    inFavor
+    proposal
+}
+`
+
+
+export const ACCOUNT_DATA = gql`
+query Proposal($id: ID!){
+    id
+    proposals
+    votes
+}
+`
+
