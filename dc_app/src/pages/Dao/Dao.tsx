2d0a83cdeac2e5453b832c2a9a711b330ecb91fa
Ryan <rdturner40@gmail.com>
2021-10-11 00:54:38 -0400

    Break up page into components, add switch component to control rendering

--- dc_app/src/pages/Dao/Dao.tsx
+++ dc_app/src/pages/Dao/Dao.tsx
@@ -1,49 +1,43 @@
-import React from 'react';
-import { AddNewBtn, InfoBox, InfoDisplayWrapper, InfoHeader, InfoSecondary, ProposalsDisplay, WhiteDisplay, Wrapper, ProposalRow, ProposalTitle, LeftProposalRow, StatusBox, ProposalDetails, Indicator, IndicatorImage } from './Style'
-import active from '../../assets/ActiveClock.svg';
+import {useState} from 'react';
+import SwitchSelector from 'react-switch-selector';
+import { Wrapper, SwitchContainer } from './Style'
+import ViewDaoProposals from 'components/ViewDaoProposals/ViewDaoProposals';
+
+const switchOptions = [
+    {
+        label: 'CREATE PROPOSAL',
+        value: 'create',
+        selectedBackgroundColor: '#01FFA5',
+    },
+    {
+        label: 'VIEW PROPOSALS',
+        value: 'view',
+        selectedBackgroundColor: '#01FFA5',
+    }
+]
 
 export default function Dao() {
+    const [selectedFunction, setSelectedFunction] = useState<string>();
+    const initialSelectedIndex = switchOptions.findIndex(({value}: any) => value === 'swap');
+
+    const handleSwitchChange = (newValue: any) => {
+        setSelectedFunction(newValue)
+    }
+
     return (
         <Wrapper>
-            <AddNewBtn>Add New Proposal</AddNewBtn>
-            <InfoDisplayWrapper>
-                <InfoBox>
-                    <InfoHeader>Active Proposals</InfoHeader>
-                    <InfoSecondary>1</InfoSecondary>
-                </InfoBox>
-                <InfoBox>
-                    <InfoHeader>Voting Addresses</InfoHeader>
-                    <InfoSecondary>100</InfoSecondary>
-                </InfoBox>
-                <InfoBox>
-                    <InfoHeader>Votes Delegated</InfoHeader>
-                    <InfoSecondary>2000</InfoSecondary>
-                </InfoBox>
-            </InfoDisplayWrapper>
-            <ProposalsDisplay>
-                <WhiteDisplay>Proposals</WhiteDisplay>
-                <ProposalRow>
-                    <LeftProposalRow>
-                        <ProposalTitle>Proposal 1</ProposalTitle>
-                        <StatusBox active>ACTIVE</StatusBox>
-                    </LeftProposalRow>
-                        <ProposalDetails>#01 Executed November 12th, 2021</ProposalDetails>
-                        <IndicatorImage src={active} />
-                    <Indicator>ACTIVE</Indicator>
-                </ProposalRow>
-                <ProposalRow>
-                    <LeftProposalRow>
-                    <ProposalTitle>Proposal 2</ProposalTitle>
-                        <StatusBox passed>PASSED</StatusBox>
-                    </LeftProposalRow>
-                </ProposalRow>
-                <ProposalRow>
-                    <LeftProposalRow>
-                        <ProposalTitle>Proposal 3</ProposalTitle>
-                        <StatusBox rejected>Rejected</StatusBox>
-                    </LeftProposalRow>
-                </ProposalRow>
-            </ProposalsDisplay>
+            <SwitchContainer>
+            <SwitchSelector
+                onChange={handleSwitchChange}
+                options={switchOptions}
+                initialSelectedIndex={initialSelectedIndex}
+                backgroundColor={'#353B3B'}
+                fontColor={'white'}
+                selectedFontColor={'black'}
+                fontSize={18}
+            />
+            </SwitchContainer>
+            {selectedFunction === 'view' && <ViewDaoProposals />}
         </Wrapper>
     )
 }
